57af72aacae5d9172c9dd0daf1e5d809440f43bd
yuanzhe <3364710079@qq.com>
2026-08-11 11:39:05 +0800

    feat: add Redis cache for retrieval results

---
+++ app/cache/retrieval_cache.py
@@ -0,0 +1,78 @@
+from app.core.redis import redis_client
+import hashlib
+import json
+
+
+class RetrievalCache:
+    PREFIX = "retrieval:"
+
+    def _make_key(
+            self,
+            owner_id,
+            kb_id,
+            query,
+            filters
+    ):
+        payload = {
+            "query": query,
+            "filters": filters,
+        }
+
+        query_hash = hashlib.md5(
+            json.dumps(
+                payload,
+                sort_keys=True,
+                ensure_ascii=False,
+            ).encode("utf-8")
+        ).hexdigest()
+
+        return (
+            f"{self.PREFIX}"
+            f"{owner_id}:"
+            f"{kb_id}:"
+            f"{query_hash}"
+        )
+
+    def get(
+            self,
+            owner_id,
+            kb_id,
+            query,
+            filters
+    ):
+        key = self._make_key(
+            owner_id,
+            kb_id,
+            query,
+            filters
+        )
+
+        value = redis_client.get(key)
+
+        if value:
+            return json.loads(value)
+
+        return None
+
+    def set(
+            self,
+            owner_id,
+            kb_id,
+            query,
+            filters,
+            result,
+            expire=3600
+    ):
+        key = self._make_key(
+            owner_id,
+            kb_id,
+            query,
+            filters
+        )
+
+        redis_client.set(
+            key,
+            json.dumps(result),
+            ex=expire,
+        )
+
--- app/services/rag_service.py
+++ app/services/rag_service.py
@@ -1,6 +1,10 @@
 from app.config import SEARCH_TOP_K
+from app.crud.knowledge_base import get_kb_by_name
 from app.prompts.rag_prompt import build_prompt
 from app.core.container import container
+from app.cache.retrieval_cache import RetrievalCache
+
+retrieve_cache = RetrievalCache()
 
 
 def retrieve_context(
@@ -10,10 +14,26 @@ def retrieve_context(
         owner_id: int,
         filters=None
 ):
+
     if filters:
         filters = filters.model_dump(
             exclude_none=True
         )
+
+    kb = get_kb_by_name(
+        db,
+        kb_name,
+        owner_id
+    )
+    cache = retrieve_cache.get(
+        owner_id,
+        kb.id,
+        query,
+        filters
+    )
+    if cache is not None:
+        print("retrieval cache hit")
+        return cache
 
     contexts = container.hybrid_retriever.retrieve(
         db,
@@ -22,6 +42,13 @@ def retrieve_context(
         owner_id=owner_id,
         top_k=SEARCH_TOP_K,
         filters=filters,
+    )
+    retrieve_cache.set(
+        owner_id,
+        kb.id,
+        query,
+        filters,
+        contexts,
     )
     return contexts
 
